ae5c019272a15e3a574d4fd546afbcd42965f500
Shvejan <shvejan20112gmail.com>
2023-02-19 07:20:51 -0500

    handled extra space

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def get_applications():
     
     userid = result[0]
         # return {"Company":li[0],"Job Position":li[1],"Status":li[2],"mail":email_top}
-    c.execute("SELECT * FROM applications WHERE company_name=? AND position_name=?", (processed_email["Company"].split(":")[-1], processed_email["Job Position"].split(":")[-1]))
+    c.execute("SELECT * FROM applications WHERE company_name=? AND position_name=?", (processed_email["Company"].split(":")[-1][1:], processed_email["Job Position"].split(":")[-1][1:]))
     current_data = c.fetchone()
     print("current_data",current_data)
     if not current_data:
@@ -73,7 +73,7 @@ def get_applications():
         c.execute('SELECT company_name, position_name, status, content FROM applications WHERE userid=?', (userid,))
         length=len(c.fetchall())
         print( (processed_email["Company"].split(":")[-1][1:],processed_email["Job Position"].split(":")[-1][1:],processed_email["Status"].split(":")[-1],userid,""))
-        c.execute("INSERT INTO applications (email_code,company_name, position_name, status,userid,content) VALUES (?,?,?,?,?,?)",(str(length+2),processed_email["Company"].split(":")[-1],processed_email["Job Position"].split(":")[-1],processed_email["Status"].split(":")[-1],userid,""))
+        c.execute("INSERT INTO applications (email_code,company_name, position_name, status,userid,content) VALUES (?,?,?,?,?,?)",(str(length+2),processed_email["Company"].split(":")[-1][1:],processed_email["Job Position"].split(":")[-1],processed_email["Status"].split(":")[-1],userid,""))
         conn.commit()
     c.execute('SELECT company_name, position_name, status, content FROM applications WHERE userid=?', (userid,))
     applications = c.fetchall()
